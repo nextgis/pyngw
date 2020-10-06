1d8024dde42d95e2580e60649969f1f6edf6a7dc
Artem Svetlov <trolleway@yandex.ru>
2020-10-06 17:58:39 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyngw", # Replace with your own username
+    version="1.0.0",
+    author="Artem Svetlov",
+    author_email="artem.svetlov@nextgis.com",
+    description="Python wraper for NextGIS Web REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nextgis/pyngw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
