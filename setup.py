ef69c10a960a9b3d05eee9e1407837bc163d6328
Artem Svetlov <trolleway@yandex.ru>
2021-08-10 12:56:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nextgis/pyngw",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'tuspy',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
